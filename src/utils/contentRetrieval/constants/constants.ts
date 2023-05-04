297dc9f1da5f316231e9a85160be8d3f6a319bac
jameson-and-java <jamesonandjava@gmail.com>
2023-05-04 00:11:41 -0400

    trying to fix posts path for deployment

--- src/utils/contentRetrieval/constants/constants.ts
+++ src/utils/contentRetrieval/constants/constants.ts
@@ -1,5 +1,5 @@
 import path from "path";
 
-const POSTS_PATH = path.join(process.cwd(), "\\src\\content\\posts");
+const POSTS_PATH = path.join(process.cwd(), "src/content/posts");
 
 export { POSTS_PATH };
