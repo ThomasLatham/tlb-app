e244106a0e784c93205ed6b4d6011987a6b09778
jameson-and-java <jamesonandjava@gmail.com>
2023-05-22 13:06:20 -0400

    11th time -- attemtpting to prevent codeblocks from extending too far on mobile

--- src/pages/posts/[id]/BlogPost.tsx
+++ src/pages/posts/[id]/BlogPost.tsx
@@ -86,7 +86,6 @@ const BlogPost: React.FC<Props> = ({ code, frontmatter }) => {
         article {
           width: 100%;
           max-width: 100%;
-          overflow: auto;
         }
       `}</style>
     </Layout>
