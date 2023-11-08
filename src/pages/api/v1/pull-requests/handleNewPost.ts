df5b21aa913c7df864cc27e978a6b374b864dc0e
jameson-and-java <jamesonandjava@gmail.com>
2023-11-08 14:14:37 -0500

    adding code to get new post ID from PR's diff

--- src/pages/api/v1/pull-requests/handleNewPost.ts
+++ src/pages/api/v1/pull-requests/handleNewPost.ts
@@ -23,11 +23,31 @@ const getNewPostId = async (payload: PullRequestEvent): Promise<string> => {
     payload.pull_request.merged === true &&
     (payload.pull_request.base.ref === "main" || payload.pull_request.base.ref === "test")
   ) {
-    console.log(await fetch(payload.pull_request.diff_url));
-    newPostId = "shmowzow";
+    newPostId = getNewPostIdFromDiff(await (await fetch(payload.pull_request.diff_url)).text());
   }
 
   return new Promise(() => newPostId);
+};
+
+const getNewPostIdFromDiff = (diff: string): string => {
+  const lines = diff.split("\n");
+  let newPostId = "";
+  const postIdPattern =
+    /^diff --git a\/src\/content\/posts\/([a-zA-Z0-9_-]+)\/\1\.mdx b\/src\/content\/posts\/\1\/\1\.mdx$/;
+  for (let i = 1; i < lines.length; i++) {
+    const currentLine = lines[i];
+    const previousLine = lines[i - 1];
+
+    if (currentLine.includes("new file mode 100644")) {
+      const postIdMatch = postIdPattern.exec(previousLine);
+      if (postIdMatch) {
+        newPostId = postIdMatch[1];
+        break;
+      }
+    }
+  }
+
+  return newPostId;
 };
 
 export { getNewPostId };
