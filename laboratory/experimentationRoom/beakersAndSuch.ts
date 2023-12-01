78a54ad48117bcaa2178dcb0049f5c2fb4d1e4fd
jameson-and-java <jamesonandjava@gmail.com>
2023-12-01 12:58:05 -0500

    cleaning up

--- laboratory/experimentationRoom/beakersAndSuch.ts
+++ laboratory/experimentationRoom/beakersAndSuch.ts
@@ -1,5 +1,4 @@
 import * as fs from "fs";
-import { Client, ContactProperties, LibraryResponse } from "node-mailjet";
 import matter from "gray-matter";
 import readingTime from "reading-time";
 
@@ -33,13 +32,6 @@ import { PostFrontmatter } from "../../src/interfaces";
  */
 const executeNewPostNotificationFlow = async (newPostId: string) => {
   const frontmatter = getFrontmatterFromPostId(newPostId);
-  const mailjet = new Client({
-    apiKey: getLocalVariableValue("MAILJET_API_KEY"),
-    apiSecret: getLocalVariableValue("MAILJET_SECRET_KEY"),
-  });
-
-  const subscribers = await getSubscribersFromPostTags(frontmatter.tags, mailjet);
-  await sendNewPostNotificationEmails(subscribers, frontmatter, newPostId, mailjet);
 };
 
 const getFrontmatterFromPostId = (postId: string): PostFrontmatter => {
@@ -49,98 +41,6 @@ const getFrontmatterFromPostId = (postId: string): PostFrontmatter => {
     wordCount: sourceText.split(/\s+/gu).length,
     readingTime: readingTime(sourceText).minutes,
   } as PostFrontmatter;
-};
-
-const getSubscribersFromPostTags = async (
-  tagsFromPost: string[],
-  mailjet: Client
-): Promise<ContactProperties.ContactData[]> => {
-  const queryData: ContactProperties.GetContactDataQueryParams = {
-    ContactsList: 10354543,
-  };
-
-  const result: LibraryResponse<ContactProperties.GetContactDataResponse> = await mailjet
-    .get("contactdata", { version: "v3" })
-    .request({}, queryData);
-
-  return result.body.Data.filter((contact: ContactProperties.ContactData) => {
-    const contactSubscribedTags = contact.Data.filter(
-      (contactProperty: ContactProperties.ContactProperty) =>
-        contactProperty.Name === "tags_subscribed_to"
-    )[0].Value.split(", ");
-
-    return (
-      contactSubscribedTags.includes("all") || hasCommonElement(tagsFromPost, contactSubscribedTags)
-    );
-  });
-};
-
-/**
- * Check if there is any common element between two arrays of strings.
- *
- * @param {string[]} array1 - The first array of strings.
- * @param {string[]} array2 - The second array of strings.
- * @returns {boolean} Returns true if there is any common element, false otherwise.
- */
-const hasCommonElement = (array1: string[], array2: string[]): boolean => {
-  // Use the Set data structure for efficient membership testing
-  const set1 = new Set(array1);
-
-  // Check if any element in array2 is in set1
-  for (const element of array2) {
-    if (set1.has(element)) {
-      return true; // Found a common element
-    }
-  }
-
-  // No common element found
-  return false;
-};
-
-const sendNewPostNotificationEmails = async (
-  subscribers: ContactProperties.ContactData[],
-  newPostFrontmatter: PostFrontmatter,
-  newPostId: string,
-  mailjet: Client
-) => {
-  const request = mailjet.post("send").request({
-    Messages: subscribers.map((subscriber) => {
-      return {
-        FromEmail: "contact@tomlatham.blog",
-        FromName: "Tom Latham",
-        Recipients: [
-          {
-            Email: getContactPropertyValue(subscriber, "email_address"),
-            Name: getContactPropertyValue(subscriber, "first_name"),
-          },
-        ],
-        Subject: "There's a new post on the Tom Latham Blog!",
-        "Mj-TemplateID": 5346495,
-        "Mj-TemplateLanguage": true,
-        Vars: {
-          newPostFrontmatter: {
-            ...newPostFrontmatter,
-          },
-          newPostId: newPostId,
-        },
-      };
-    }),
-  });
-  request
-    .then((result) => {
-      console.log(result.body);
-    })
-    .catch((error) => {
-      console.log(error.statusCode);
-    });
-};
-
-const getContactPropertyValue = (
-  contactData: ContactProperties.ContactData,
-  propertyName: string
-) => {
-  return contactData.Data.filter((contactProperty) => contactProperty.Name === propertyName)[0]
-    .Value;
 };
 
 /* *** *** *** *** *** *** *** *** *** *** *** *** *
--- src/pages/api/v1/pull-requests.ts
+++ src/pages/api/v1/pull-requests.ts
@@ -5,7 +5,6 @@ import * as crypto from "crypto";
 import matter from "gray-matter";
 import * as fs from "fs";
 import readingTime from "reading-time";
-import { Client, ContactProperties, LibraryResponse } from "node-mailjet";
 
 import { POSTS_PATH } from "@/constants";
 import { PostFrontmatter } from "@/interfaces";
@@ -139,105 +138,6 @@ const getFrontmatterFromPostId = (postId: string): PostFrontmatter => {
 
 const executeNewPostNotificationFlow = async (newPostId: string) => {
   const frontmatter = getFrontmatterFromPostId(newPostId);
-  const mailjet = new Client({
-    apiKey: process.env.MAILJET_API_KEY,
-    apiSecret: process.env.MAILJET_SECRET_KEY,
-  });
-  // new flow now that we can't use segmentation:
-  // - going to use the Send API (v3) rather than campaigns
-  // - get all the subscribers, then filter here (NextJS) for those subscribed to
-};
-
-const getSubscribersFromPostTags = async (
-  tagsFromPost: string[],
-  mailjet: Client
-): Promise<ContactProperties.ContactData[]> => {
-  const queryData: ContactProperties.GetContactDataQueryParams = {
-    ContactsList: 10354543,
-  };
-
-  const result: LibraryResponse<ContactProperties.GetContactDataResponse> = await mailjet
-    .get("contactdata", { version: "v3" })
-    .request({}, queryData);
-
-  return result.body.Data.filter((contact: ContactProperties.ContactData) => {
-    const contactSubscribedTags = contact.Data.filter(
-      (contactProperty: ContactProperties.ContactProperty) =>
-        contactProperty.Name === "tags_subscribed_to"
-    )[0].Value.split(", ");
-
-    return (
-      contactSubscribedTags.includes("all") || hasCommonElement(tagsFromPost, contactSubscribedTags)
-    );
-  });
-};
-
-/**
- * Check if there is any common element between two arrays of strings.
- *
- * @param {string[]} array1 - The first array of strings.
- * @param {string[]} array2 - The second array of strings.
- * @returns {boolean} Returns true if there is any common element, false otherwise.
- */
-const hasCommonElement = (array1: string[], array2: string[]): boolean => {
-  // Use the Set data structure for efficient membership testing
-  const set1 = new Set(array1);
-
-  // Check if any element in array2 is in set1
-  for (const element of array2) {
-    if (set1.has(element)) {
-      return true; // Found a common element
-    }
-  }
-
-  // No common element found
-  return false;
-};
-
-const sendNewPostNotificationEmails = async (
-  subscribers: ContactProperties.ContactData[],
-  newPostFrontmatter: PostFrontmatter,
-  newPostId: string,
-  mailjet: Client
-) => {
-  const request = mailjet.post("send").request({
-    Messages: subscribers.map((subscriber) => {
-      return {
-        FromEmail: "contact@tomlatham.blog",
-        FromName: "Tom Latham",
-        Recipients: [
-          {
-            Email: getContactPropertyValue(subscriber, "email_address"),
-            Name: getContactPropertyValue(subscriber, "first_name"),
-          },
-        ],
-        Subject: "There's a new post on the Tom Latham Blog!",
-        TemplateID: 5346495,
-        "Mj-TemplateLanguage": true,
-        Vars: {
-          newPostFrontmatter: {
-            ...newPostFrontmatter,
-          },
-          newPostId: newPostId,
-        },
-      };
-    }),
-  });
-  request
-    .then((result) => {
-      console.log(result.body);
-    })
-    .catch((error) => {
-      console.log(error.statusCode);
-    });
-};
-
-const getContactPropertyValue = (
-  contactData: ContactProperties.ContactData,
-  propertyName: string
-) => {
-  return contactData.Data.filter((contactProperty) => contactProperty.Name === propertyName)[0]
-    .Value;
 };
 
 export default handlePullRequest;
