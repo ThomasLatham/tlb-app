a259273a244bc512e90db869055d4991cf67d0ce
jameson-and-java <jamesonandjava@gmail.com>
2023-12-05 14:05:29 -0500

    trying auth with google

--- src/pages/subscribe/SubscribePage.tsx
+++ src/pages/subscribe/SubscribePage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Head from "next/head";
 import { signIn, useSession } from "next-auth/react";
 
@@ -14,10 +14,32 @@ const SubscribePage: React.FC = () => {
   return (
     <Layout>
       <Head key="subscribe">
-        <title>{"Subscribe"}</title>
+        <title>{"Subscription Settings"}</title>
       </Head>
-      <div>
-        <ButtonBasic onClick={() => signIn()} text="Sign In" />
+      <div
+        className="
+        flex flex-col items-center content-center justify-center 
+        dark:text-trim-dark text-secondary-light 
+        max-w-full"
+      >
+        <p className="mt-6 text-[30px] underline">Subscription Settings</p>
+        {status !== "authenticated" ? (
+          <Fragment>
+            <p className="sm:pl-5 sm:mt-6 mt-6 text-justify">
+              Please sign in to subscribe and to tailor your subscription settings.
+            </p>
+            <div className="sm:pl-5 sm:mt-2 mt-2 text-justify">
+              <span className="mr-4">
+                <ButtonBasic onClick={() => signIn("google")} text="Sign in with Google" />
+              </span>
+              <span>
+                <ButtonBasic onClick={() => signIn("email")} text="Sign in with Email" />
+              </span>
+            </div>
+          </Fragment>
+        ) : (
+          <div></div>
+        )}
       </div>
     </Layout>
   );
